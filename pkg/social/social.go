8cf991b2dfeee331a60a2f92dc261c146e114ced
ryan <ryantxu@gmail.com>
2018-02-07 11:18:05 +0100

    put back google auth login stuff

--- pkg/social/social.go
+++ pkg/social/social.go
@@ -74,6 +74,7 @@ func NewOAuthService() {
 			TlsClientKey:   sec.Key("tls_client_key").String(),
 			TlsClientCa:    sec.Key("tls_client_ca").String(),
 			TlsSkipVerify:  sec.Key("tls_skip_verify_insecure").MustBool(),
+			RedirectUrl:    sec.Key("redirect_url").String(),
 		}
 
 		if !info.Enabled {
@@ -93,7 +94,7 @@ func NewOAuthService() {
 				AuthURL:  info.AuthUrl,
 				TokenURL: info.TokenUrl,
 			},
-			RedirectURL: strings.TrimSuffix(setting.AppUrl, "/") + SocialBaseUrl + name,
+			RedirectURL: info.RedirectUrl,
 			Scopes:      info.Scopes,
 		}
 
@@ -151,8 +152,7 @@ func NewOAuthService() {
 					AuthURL:  setting.GrafanaComUrl + "/oauth2/authorize",
 					TokenURL: setting.GrafanaComUrl + "/api/oauth2/token",
 				},
-				RedirectURL: strings.TrimSuffix(setting.AppUrl, "/") + SocialBaseUrl + name,
-				Scopes:      info.Scopes,
+				Scopes: info.Scopes,
 			}
 
 			SocialMap["grafana_com"] = &SocialGrafanaCom{
@@ -164,6 +164,10 @@ func NewOAuthService() {
 				allowSignup:          info.AllowSignup,
 				allowedOrganizations: util.SplitString(sec.Key("allowed_organizations").String()),
 			}
+		}
+
+		if len(config.RedirectURL) == 0 {
+			config.RedirectURL = strings.TrimSuffix(setting.AppUrl, "/") + SocialBaseUrl + name
 		}
 	}
 }
