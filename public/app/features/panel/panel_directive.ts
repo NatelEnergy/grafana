546fe02ff9258db33b961939193c709eb9c1f26e
ryan <ryantxu@gmail.com>
2018-05-08 11:14:11 +0200

    wrap again

--- public/app/features/panel/panel_directive.ts
+++ public/app/features/panel/panel_directive.ts
@@ -21,7 +21,9 @@ var panelTemplate = `
     </div>
 
     <div class="panel-content">
-      <ng-transclude class="panel-height-helper"></ng-transclude>
+      <div class="panel-height-helper">
+        <ng-transclude class="panel-height-helper"></ng-transclude>
+      </div>
     </div>
   </div>
 
@@ -113,7 +115,7 @@ module.directive('grafanaPanel', function($rootScope, $document, $timeout) {
           `;
 
           let scrollRoot = panelContent;
-          let scroller = panelContent.find(':first').find(':first');
+          let scroller = panelContent.find(':first');
 
           // Save the wrapper div to check content height after render
           if (ctrl.panel.dynamicHeight) {
