aa136a7d3d7a4ee32395710478057e9d5e579097
ryan <ryantxu@gmail.com>
2018-05-16 09:22:51 +0200

    add render: hack for rules

--- pkg/services/alerting/notifier.go
+++ pkg/services/alerting/notifier.go
@@ -93,6 +93,11 @@ func (n *notificationService) uploadImage(context *EvalContext) (err error) {
 	}
 	renderOpts.Path = fmt.Sprintf("d-solo/%s/%s?panelId=%d", ref.Uid, ref.Slug, context.Rule.PanelId)
 
+	if strings.HasPrefix(context.Rule.Message, "RENDER:") {
+		renderOpts.Path = scontext.Rule.Message[:len("RENDER:")]
+	}
+	n.log.Info("alert-rendering", "path", renderOpts.Path)
+
 	imagePath, err := renderer.RenderToPng(renderOpts)
 	if err != nil {
 		return err
